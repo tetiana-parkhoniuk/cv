cde1405eb6dc220352877599694a99e89652d6e2
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-11-16 23:10:52 +0200

    Adds CV markup, desktop styles and minimum js.

---
+++ index.js
@@ -0,0 +1,12 @@
+const downloadLink = document.querySelector('.downloadLink');
+
+const thankYouParagraph = document.createElement('p');
+const paragraphContent = document.createTextNode('Thank you for downloading!');
+thankYouParagraph.appendChild(paragraphContent);
+thankYouParagraph.style.cssText = 'margin-top: 10px; color:#283593;';
+
+function onClick() {
+  downloadLink.insertAdjacentElement('afterend', thankYouParagraph);
+}
+
+downloadLink.addEventListener('click', onClick);
