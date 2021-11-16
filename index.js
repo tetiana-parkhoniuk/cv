fb0f83b554367d75ef8e2917a0e7c25f36cada3a
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-11-16 23:32:32 +0200

    Adds link to contact.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const downloadLink = document.querySelector('.downloadLink');
 const thankYouParagraph = document.createElement('p');
 const paragraphContent = document.createTextNode('Thank you for downloading!');
 thankYouParagraph.appendChild(paragraphContent);
-thankYouParagraph.style.cssText = 'margin-top: 10px; color:#283593;';
+thankYouParagraph.style.cssText =
+  'margin-top: 10px; margin-right: 10px; color:#283593;';
 
 function onClick() {
   downloadLink.insertAdjacentElement('afterend', thankYouParagraph);
